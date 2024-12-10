f9a2f9b7649a7644b1b5ccb9600a00f7e1221f92
HitalloFeitosa <hitallo.feitosa@oficialfarma.com.br>
2024-12-09 21:40:44 -0300

    feat: add initial files

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Header from './components/Header/Header';
+import LandingPage from './pages/LandingPage';
+function App() {
+  return (
+    <>
+      <Header/>
+      <LandingPage />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './header.css';
+export default function Header() {
+  return (
+    <header>
+        <div className='header-content'>
+            <span>Tech<strong>Bits</strong></span>
+        </div>
+    </header>
+  )
+}
