3de6e6a99b405f3c3209e93af6af3f9da864e566
HitalloFeitosa <hitallo.feitosa@oficialfarma.com.br>
2024-12-10 21:38:08 -0300

    feat: add landing page and change header style

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -3,6 +3,13 @@ import './landing.css';
 
 export default function LandingPage() {
   return (
-    <div className='container'>LandingPage</div>
+    <div className='container content'>
+      <div className="about">
+        <h1>TechBits</h1>
+        <p>Welcome to TechBits Store, your go-to online shop for top-tier hardware components! Whether you're building a custom PC, upgrading your gaming rig, or enhancing your office setup, we've got you covered. Explore a wide range of products, from high-performance GPUs and motherboards to reliable power supplies and lightning-fast SSDs.
+          With competitive prices, secure shopping, and fast delivery, BoostTech Store ensures a seamless experience from click to doorstep. Upgrade your tech game today—because quality hardware makes all the difference!</p>
+          <button>Get Started</button>
+      </div>
+    </div>
   )
 }
