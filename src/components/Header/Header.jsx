2eba29a2f28a388e70d5b3001668565daac0c33a
HitalloFeitosa <hitallo.feitosa@oficialfarma.com.br>
2024-12-14 13:41:16 -0300

    feat: add condition on header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,29 +1,31 @@
 import React from 'react';
 import './header.css';
 import cart from '../../../public/cart.png';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, useLocation } from 'react-router-dom';
 export default function Header() {
   const navigate = useNavigate();
+  const location = useLocation();
 
-  
   const handleGoToHome = () => {
     navigate('/');
   };
 
   const handleCartAction = () => {
-      navigate('/cart');
+    navigate('/cart');
   };
 
   return (
     <header>
-        <div className='header-container'>
-          <div className='logo'>
-            <span onClick={handleGoToHome}>Plantify</span>
-          </div>
-            <div onClick={handleCartAction}>
-              <img src={cart} alt="minicart" />
-            </div>
+      <div className='header-container'>
+        <div className='logo'>
+          <span onClick={handleGoToHome}>Plantify</span>
         </div>
+        {location.pathname !== '/' && (
+          <div onClick={handleCartAction}>
+            <img src={cart} alt="minicart" />
+          </div>
+        )}
+      </div>
     </header>
   )
 }
